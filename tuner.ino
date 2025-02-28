efe2e982fae886e405cfa46b384e5bab394d3797
StefanescuRazvan <razvan_stefanescu27@yahoo.ro>
2025-02-28 17:01:03 +0200

    Add files via upload

---
+++ tuner.ino
@@ -0,0 +1,66 @@
+#include <driver/i2s.h>
+#include <arduinoFFT.h>
+
+#define I2S_WS  15  // Word Select (L/R)
+#define I2S_SCK 14  // Bit Clock
+#define I2S_SD  32  // Data In
+
+#define SAMPLES 1024  // Trebuie să fie putere a lui 2
+#define SAMPLE_RATE 44100 // Rată de eșantionare
+
+double vReal[SAMPLES];  
+double vImag[SAMPLES];  
+
+ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, SAMPLES, SAMPLE_RATE);
+
+void setup() {
+    Serial.begin(115200);
+
+    // Configurare I2S
+    i2s_config_t i2s_config = {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+        .sample_rate = SAMPLE_RATE,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = I2S_COMM_FORMAT_I2S,
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+        .dma_buf_count = 8,
+        .dma_buf_len = 1024,
+        .use_apll = false
+    };
+
+    i2s_pin_config_t pin_config = {
+        .bck_io_num = I2S_SCK,
+        .ws_io_num = I2S_WS,
+        .data_out_num = -1,
+        .data_in_num = I2S_SD
+    };
+
+    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_NUM_0, &pin_config);
+}
+
+void loop() {
+    size_t bytesRead;
+    int32_t samples[SAMPLES];
+
+    // Citim datele de la microfon
+    i2s_read(I2S_NUM_0, &samples, sizeof(samples), &bytesRead, portMAX_DELAY);
+
+    for (int i = 0; i < SAMPLES; i++) {
+        vReal[i] = samples[i] / 2147483648.0;  // Normalizare la [-1,1]
+        vImag[i] = 0;
+    }
+
+    // Aplicăm FFT
+    FFT.windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD); // Fereastră Hamming
+    FFT.compute(FFT_FORWARD);                       // Transformare Fourier
+    FFT.complexToMagnitude();                       // Magnitudine spectru
+
+    // Detectăm frecvența dominantă
+    double peakFrequency = FFT.majorPeak();
+    //Serial.print("Frecvență detectată: ");
+    Serial.println(peakFrequency);
+
+    delay(1000);
+}
